5fe0397bb56ba22d8cd1c49a7f25803da1f304af
Amani Hassan <amanihas2021@gmail.com>
2025-02-20 18:13:26 -0500

    cname

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
 
         {/* Detailed Bio */}
         <p className="about-description">
-          I’m a senior at the University of Central Florida pursuing a B.S. in Human Factors Psychology with a Minor in Computer Science & Information Technology. As an aspiring full-stack developer, I specialize in React, Java, and C to bring web applications to life.
+          I’m a senior at the University of Central Florida pursuing a B.S. in Human Factors Psychology with Minors in Computer Science & Information Technology. As an aspiring full-stack developer, I specialize in React, Java, and C to bring web applications to life.
         </p>
         <p className="about-description">
           My studies in computer science have equipped me with a strong foundation in programming, system design, and problem-solving, while my background in psychology provides insight into user behavior. This unique combination enables me to design accessible, intuitive, and visually engaging UI/UX experiences. Whether I’m building dynamic front-end interfaces or optimizing back-end systems, my goal is to create digital solutions that are both functional and user-centered.
