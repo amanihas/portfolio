b6f4d06e3fb13620fec233d4f88cb6b3d4199ce8
Amani Hassan <amanihas2021@gmail.com>
2025-05-27 12:04:20 -0400

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,14 +24,15 @@ const About = () => {
 
         {/* Detailed Bio */}
         <p className="about-description">
-        I’m a senior at the University of Central Florida, pursuing a B.S. in Human Factors Psychology with Minors in Computer Science & Information Technology. I began my college journey as with a focus on psychology, but halfway through, I discovered my passion for technology and switched my focus to Computer Science. This decision has allowed me to blend my deep understanding of human behavior with the creative challenge of building engaging digital experiences.
+          I recently graduated from the University of Central Florida with a B.S. in Human Factors Psychology and minors in Computer Science and Information Technology. I began my college journey focused on psychology, but halfway through, I discovered a passion for technology and shifted my focus to Computer Science. This path has allowed me to blend a deep understanding of human behavior with the creative challenge of building engaging digital experiences.
         </p>
         <p className="about-description">
-        I am an aspiring UX/UI designer. My background in psychology gives me insights into user behavior, while my studies in computer science have provided me with a strong foundation in programming, system design, and problem-solving. I work with technologies like React, Java, and C to bring dynamic web applications to life.
+          I’m an aspiring UX/UI designer. My background in psychology provides valuable insights into user behavior, while my studies in computer science have given me a strong foundation in programming, system design, and problem-solving. I work with technologies like React, Java, and C to bring dynamic web applications to life.
         </p>
         <p className="about-description">
-        Though I may not have years of experience, my passion, dedication, and willingness to work hard drive me to continuously learn and grow. I’m excited to create digital solutions that are both functional and user-centered, always striving to make technology accessible and enjoyable for everyone.
+          While I’m at the start of my professional journey, my passion, dedication, and eagerness to learn drive me to grow continuously. I’m excited to create digital solutions that are both functional and user-centered, always striving to make technology accessible and enjoyable for everyone.
         </p>
+
 
         {/* Social Links with Icons */}
         <div className="about-social">
